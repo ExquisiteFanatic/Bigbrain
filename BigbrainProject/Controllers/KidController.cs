250767bebd19d48d374e50033efb6c9a2ee8f4bb
ExquisiteFanatic <goldenmember334@gmail.com>
2022-04-21 21:13:20 +0300

    Learning CURVE

---
+++ BigbrainProject/Controllers/KidController.cs
@@ -0,0 +1,25 @@
+﻿using BigbrainProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigbrainProject.Controllers
+{
+    public class KidController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(KidModel model)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BigbrainProject/Models/KidModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BigbrainProject.Models
+{
+    public class KidModel
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
